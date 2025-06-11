ab12162bfed52a7637b89f5db20364fdfd1ed375
OlegWhiteRose <hkikomori@yandex.ru>
2025-06-11 10:56:00 +0300

    new

---
+++ lab4/example-nestjs/src/stocks/stocks.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@nestjs/common';
+import { FileService } from 'src/file/file.service';
+import { Stock } from './entities/stock.entity';
+import { CreateStockDto } from './dto/create-stock.dto';
+import { UpdateStockDto } from './dto/update-stock.dto';
+
+@Injectable()
+export class StocksService {
+  constructor(private fileService: FileService<Stock[]>) {
+    this.fileService.setFilePath('../../assets/stocks.json');
+  }
+
+  create(createStockDto: CreateStockDto) {
+    const stocks = this.fileService.read();
+    const stock = { ...createStockDto, id: stocks.length + 1 };
+    this.fileService.add(stock);
+  }
+
+  findAll(title?: string): Stock[] {
+    const stocks = this.fileService.read();
+    return title
+      ? stocks.filter((stock) =>
+          stock.title.toLowerCase().includes(title.toLowerCase()),
+        )
+      : stocks;
+  }
+
+  findOne(id: number): Stock | null {
+    const stocks = this.fileService.read();
+    return stocks.find((stock) => stock.id === id) ?? null;
+  }
+
+  update(id: number, updateStockDto: UpdateStockDto): void {
+    const stocks = this.fileService.read();
+    const updatedStocks = stocks.map((stock) =>
+      stock.id === id ? { ...stock, ...updateStockDto } : stock,
+    );
+    this.fileService.write(updatedStocks);
+  }
+
+  remove(id: number): void {
+    const filteredStocks = this.fileService.read().filter((stock) => stock.id !== id);
+    this.fileService.write(filteredStocks);
+  }
+}
+
